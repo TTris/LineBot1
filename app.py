3d29277de94c7689ec4ff7f40fc4052d7f747ae7
TTris725 <mctsai725@gmail.com>
2025-03-19 21:36:00 +0800

    Forth Day - Buttons + Carousel + ImageCarousel Template

--- app.py
+++ app.py
@@ -95,8 +95,122 @@ def handle_message(event):
                 )
             )
 
+        # Buttons Template
+        elif text == "Buttons":
+            url = request.url_root + "/static/S__108437507.jpg"
+            url = url.replace("http", "https")
+            app.logger.info("url=" + url)
+            buttons_template = ButtonsTemplate(
+                thumbnailImageUrl = url,
+                title = "示範",
+                text = "這是一塊雞排",
+                actions=[
+                    URIAction(label="連結", uri="https://youtu.be/Mw3cODdkaFM?si=x7-LdpCpeVcBu7r2"),
+                    PostbackAction(label="回傳值", data="ping", displayText="傳了"),
+                    MessageAction(label="傳'哈囉'", text="哈囉"),
+                    DatetimePickerAction(label="選擇時間", data="時間", mode="datetime")
+                ])
+            template_message = TemplateMessage(
+                alt_text = "This is a buttons template.",
+                template=buttons_template
+            )
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    replyToken=event.reply_token,
+                    messages=[template_message]
+                )
+            )
+        
+        # Carousel Template
+        elif text == "Carousel":
+            url = request.url_root + "/static/S__1563025412.jpg"
+            url = url.replace("http", "https")
+            app.logger.info("url=" + url)
+            carousel_template = CarouselTemplate(
+                columns=[
+                    CarouselColumn(
+                        thumbnailImageUrl=url,
+                        title="第一項",
+                        text="這是第一項的描述",
+                        actions=[
+                            URIAction(
+                                label="點我前往Google",
+                                uri="https://www.google.com"
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnailImageUrl=url,
+                        title="第二項",
+                        text="這是第二項的敘述",
+                        actions=[
+                            URIAction(
+                                label="點我前往Youtube",
+                                uri="https://www.youtube.com"
+                            )
+                        ]
+                    )                         
+                ]
+            )
+            carousel_message = TemplateMessage(
+                altText="這是Carousel Template",
+                template=carousel_template
+            )
 
-        if text == "文字":
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    replyToken=event.reply_token,
+                    messages=[carousel_message]
+                )
+            )
+        
+        # ImageCarousel Template
+        elif text == "Image Carousel":
+            url = request.url_root + "/static"
+            url = url.replace("http", "https")
+            app.logger.info("url=" + url)
+            image_carousel_template = ImageCarouselTemplate(
+                columns=[
+                    ImageCarouselColumn(
+                        imageUrl= url + "/IMG_5575.jpg",
+                        action=URIAction(
+                            label="維基小貓咪",
+                            uri="https://en.wikipedia.org/wiki/Cat"
+                        )
+                    ),
+                    ImageCarouselColumn(
+                        imageUrl= url + "/IMG_5975.jpg",
+                        action=URIAction(
+                            label="YT小貓咪",
+                            uri="https://www.youtube.com/results?search_query=cat"
+                        )
+                    ),
+                    ImageCarouselColumn(
+                        imageUrl= url + "/S__112836713.jpg",
+                        action=URIAction(
+                            label="貓屎咖啡",
+                            uri="https://en.wikipedia.org/wiki/Kopi_luwak"
+                            
+                        )
+                    )
+                ]
+            )
+
+            image_carousel_message = TemplateMessage(
+                altText="圖片輪播範本",
+                template=image_carousel_template
+            )
+
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    replyToken=event.reply_token,
+                    messages=[image_carousel_message]
+                )
+            )
+
+
+
+        elif text == "文字":
             line_bot_api.reply_message(
                 ReplyMessageRequest(
                     replyToken = event.reply_token,
